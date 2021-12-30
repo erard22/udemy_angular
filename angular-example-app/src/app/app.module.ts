efe6a93b02806afe651113e82602791c2a2b8177
Michel Erard <michel.erard@gmail.com>
2021-12-30 20:50:21 +0100

    added redux dev-tools

--- angular-example-app/src/app/app.module.ts
+++ angular-example-app/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthEffects } from './auth/store/auth.effects';
-import { authReducer } from './auth/store/auth.reducer';
 import { CoreModule } from './core.module';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from './shared/shared.module';
-import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 import * as fromApplication from './store/app.reducer';
 
 @NgModule({
@@ -25,6 +25,7 @@ import * as fromApplication from './store/app.reducer';
     HttpClientModule,
     StoreModule.forRoot(fromApplication.applicationReducers),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({logOnly: environment.production}),
     SharedModule,
     CoreModule
   ],
